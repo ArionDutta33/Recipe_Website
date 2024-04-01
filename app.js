5748705608892e39ce712bfeee25488b2368048f
ArionDutta33 <ariondutta333@outlook.com>
2024-04-01 21:43:12 +0530

    Added the cooking instructions to the pop up

--- app.js
+++ app.js
@@ -10,11 +10,12 @@ const recipeContainer = document.querySelector(".recipeContainer")
 const getRecipes = async (query) => {
     const data = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`)
     const response = await data.json()
-    console.log(response)
+    // console.log(response)
     console.log(response.meals)
     recipeContainer.innerHTML = ""
 
     for (let meals of response.meals) {
+        // console.log(meals, "melas")
         const newRecipe = document.createElement("div")
         newRecipe.innerHTML = `
         <div class="img-box">
@@ -26,8 +27,32 @@ const getRecipes = async (query) => {
         `
         newRecipe.classList.add("recipe-box")
         recipeContainer.appendChild(newRecipe)
+
+        const showBtn = newRecipe.querySelector(".viewRecipe")
+        showBtn.addEventListener("click", () => {
+            openPopup(meals)
+        })
     }
 
+}
+const openPopup = (meals) => {
+
+    const popMenu = document.createElement("div")
+    popMenu.classList.add("popup")
+    console.log(popMenu)
+
+    popMenu.innerHTML = `<i class="fa-solid fa-x"></i>
+    <p class="instructions">${meals.strInstructions}</p>
+    `
+    recipeContainer.appendChild(popMenu)
+    const popupContainer = document.querySelector(".popup")
+    const closeBtn = popupContainer.querySelector("i")
+    console.log(closeBtn)
+
+    closeBtn.addEventListener("click", () => {
+        popupContainer.style.dispay = "none"
+        console.log("logged")
+    })
 }
 
 
